2cc6adcf0088d01c29f73b4b8de8266e217c751b
lfborjas <luis.borjas@escolarea.com>
2011-01-31 01:30:46 -0600

    Gotta learn about defmacro-bang

--- lol/chapter3.lisp
+++ lol/chapter3.lisp
@@ -82,3 +82,67 @@
               (t ,neg))))
 
 ;alternative, capture-safe, implementation
+(defmacro nif (expr pos zero neg)
+   (let ((g (gensym)))
+     `(let ((,g ,expr))
+        (cond ((plusp ,g) ,pos)
+              ((zerop ,g) ,zero)
+              (t ,neg)))))
+
+;gensyms disappear in macro expansion, that's cool
+
+(defun g!-symbol-p (s)
+    (and (symbolp s)
+         (> (length (symbol-name s) 2)
+         (string= (symbol-name s)
+                  "G!"
+                  :start1 0
+                  :end1 2))))
+
+;this macro automatically generates gensyms for every g!symbol we have
+;note that flatten is a function defined by P. Graham in "on lisp"
+(defun flatten (x)
+  (labels ((rec (x acc)
+        (cond ((null x) acc)
+              ((atom x) (cons x acc))
+              (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
+
+(defmacro defmacro/g! (name args &rest body)
+    (let ((syms (remove-duplicates 
+                    (remove-if-not #'g!-symbol-p (flatten body)))))
+       `(defmacro ,name ,args
+            (let ,(mapcar
+                    (lambda (s)
+                        `(,s (gensym ,(subseq 
+                                        (symbol-name s)
+                                        2))))
+                    syms)
+             ,@body))))
+
+;we could, then, rewrite nif as follows:
+(defmacro/g! nif (expr pos zero neg)
+  `(let ((,g!result ,expr))
+     (cond ((plusp ,g!result) ,pos)
+           ((zerop ,g!result) ,zero)
+            (t ,neg))))
+
+;an examination of Norvig's concept of once-only
+
+(defun o!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "O!"
+                :start1 0
+                :end1 2)))
+
+;this is another PG utility
+(defun symb (&rest args)
+  (values (intern (apply #'mkstr args))))
+
+(defun o!-symbol-to-g!-symbol (s)
+  (symb "G!"
+        (subseq (symbol-name s) 2)))
+
+
