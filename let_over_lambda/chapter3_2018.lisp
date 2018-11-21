31e9fc8403cc17d50d7070359ba497b58e0a0245
Luis Borjas <luisfborjas@gmail.com>
2018-11-21 00:36:48 -0500

    done with chapter 3

--- let_over_lambda/chapter3_2018.lisp
+++ let_over_lambda/chapter3_2018.lisp
@@ -59,6 +59,12 @@
                 :start1 0
                 :end1 2)))
 
+
+;; a macro-defining macro:
+;; finds and g!-symbols (e.g. g!result, g!first) and declares gemsyms
+;; bound to them in a let, which are then available in the macro,
+;; similar to Clojure's autogensym capabilities or Arc's "w-uniq"
+;; https://arclanguage.github.io/ref/macro.html
 (defmacro defmacro/g! (name args &rest body)
   (let ((syms (remove-duplicates
                (remove-if-not #'g!-symbol-p
@@ -71,3 +77,89 @@
                                2))))
               syms)
          ,@body))))
+
+
+;; (macroexpand-1
+;;  `(defmacro/g! nif (expr pos zero neg)
+;;     `(let ((,g!result ,expr))
+;;        (cond ((plusp ,g!result) ,pos)
+;;              ((zerop ,g!result) ,zero)
+;;              (t ,neg)))))
+
+;; will yield
+
+;; (DEFMACRO NIF (EXPR POS ZERO NEG)
+;;   (LET ((G!RESULT (GENSYM "RESULT")))
+;;     `(LET ((,G!RESULT ,EXPR))
+;;        (COND ((PLUSP ,G!RESULT) ,POS) ((ZEROP ,G!RESULT) ,ZERO) (T ,NEG)))))
+
+
+
+(defun o!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "O!"
+                :start1 0
+                :end1   2)))
+
+(defun o!-symbol-p-to-g!-symbol (s)
+  (symb "G!"
+        (subseq (symbol-name s) 2)))
+
+(defmacro defmacro! (name args &rest body)
+  (let* ((os (remove-if-not #'o!-symbol-p args))
+         (gs (mapcar #'o!-symbol-p-to-g!-symbol os)))
+    `(defmacro/g! ,name ,args
+       `(let ,(mapcar #'list (list ,@gs) (list ,@os))
+          ,(progn ,@body)))))
+
+;; Given the above, this
+
+;; (macroexpand '(defmacro! nif (o!expr pos zero neg)
+;;                  `(cond ((plusp ,g!expr) ,pos)
+;;                         ((zerop ,g!expr) ,zero)
+;;                         (t ,neg))))
+
+;; yields
+
+;; (macroexpand-1 '(DEFMACRO/G! NIF (O!EXPR POS ZERO NEG)
+;;                  `(LET ,(MAPCAR #'LIST (LIST G!EXPR) (LIST O!EXPR))
+;;                     ,(PROGN
+;;                       `(COND ((PLUSP ,G!EXPR) ,POS) ((ZEROP ,G!EXPR) ,ZERO)
+;;                              (T ,NEG))))))
+
+;; which in turn yields:
+
+;; (DEFMACRO NIF (O!EXPR POS ZERO NEG)
+;;   (LET ((G!EXPR (GENSYM "EXPR")))
+;;     `(LET ,(MAPCAR #'LIST (LIST G!EXPR) (LIST O!EXPR))
+;;        ,(PROGN
+;;          `(COND ((PLUSP ,G!EXPR) ,POS) ((ZEROP ,G!EXPR) ,ZERO) (T ,NEG))))))
+
+;;; For a simpler set of macros, from the book:
+
+;; (defmacro! square (o!x)
+;;   `(* ,g!x ,g!x))
+
+;; (macroexpand '(square (incf x)))
+
+;;; yielding:
+
+
+;; (LET ((#1=#:X660 (INCF X)))
+;;   (* #1# #1#))
+
+;; (defmacro! square2 (o!x)
+;;   `(progn
+;;      (format t "[~a gave ~a]~%" ',o!x ,g!x)
+;;      (* ,g!x ,g!x)))
+
+;; (macroexpand '(square2 (incf y)))
+
+;; (LET ((#1=#:X666 #2=(INCF Y)))
+;;   (PROGN (FORMAT T "[~a gave ~a]~%" '#2# #1#) (* #1# #1#)))
+
+;; CL-USER> (square2 (incf y))
+;; [(INCF Y) gave 5]
+;; 25
--- let_over_lambda/utils.lisp
+++ let_over_lambda/utils.lisp
@@ -1,8 +1,8 @@
+;;; functions and macros referenced throughout the book:
+
 ;; Defined in Chapter 1
 ;; utility functions from Graham's On Lisp
-
-
-(defun mkstr. (&rest args)
+(defun mkstr (&rest args)
   (with-output-to-string (s)
     (dolist (a args) (princ a s))))
 
@@ -21,9 +21,14 @@
                     (cons source acc))))))
     (if source (rec source nil) nil)))
 
-(defun flatten. (x)
+(defun flatten (x)
   (labels ((rec (x acc)
              (cond ((null x) acc)
+                   ;; The SBCL implementation of comma made quoted expressions flatten weird, hence the addition of this here:
+                   ;; https://github.com/thephoeron/let-over-lambda/blob/f321d9524b749730d3e40ed214ec5026750d40f9/let-over-lambda.lisp#L65
+                   ;; and some nerds talking about it here:
+                   ;; https://www.reddit.com/r/learnlisp/comments/6bdhrq/sbcl_doesnt_flatten_quasiquote_commas_the_way/
+                   ((typep x 'sb-impl::comma) (rec (sb-impl::comma-expr x) acc))
                    ((atom x) (cons x acc))
                    (t (rec
                        (car x)
@@ -45,3 +50,47 @@
 ;; CL-USER> 
 
 ;; also, C-c C-d d is very useful for looking up the documentation of certain fns!
+
+;;; Defined in Chapter 3
+
+;; see if a symbol is a "g-bang" symbol
+;; standardizing how we "name" our gensyms: any symbol that starts with g! and is followed by at least another character.
+(defun g!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "G!"
+                :start1 0
+                :end1 2)))
+
+(defmacro defmacro/g! (name args &rest body)
+  (let ((syms (remove-duplicates
+               (remove-if-not #'g!-symbol-p
+                              (flatten body)))))
+    `(defmacro ,name ,args
+       (let ,(mapcar
+              (lambda (s)
+                `(,s (gensym ,(subseq
+                               (symbol-name s)
+                               2))))
+              syms)
+         ,@body))))
+
+(defun o!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "O!"
+                :start1 0
+                :end1   2)))
+
+(defun o!-symbol-p-to-g!-symbol (s)
+  (symb "G!"
+        (subseq (symbol-name s) 2)))
+
+(defmacro defmacro! (name args &rest body)
+  (let* ((os (remove-if-not #'o!-symbol-p args))
+         (gs (mapcar #'o!-symbol-p-to-g!-symbol os)))
+    `(defmacro/g! ,name ,args
+       `(let ,(mapcar #'list (list ,@gs) (list ,@os))
+          ,(progn ,@body)))))
