48b382e57be37e1f19a4efa5bd19f3b8b482fb46
Luis Borjas <luisfborjas@gmail.com>
2018-11-24 17:19:13 -0500

    svg helper macros

--- land_of_lisp/dice_of_doom/svg.lisp
+++ land_of_lisp/dice_of_doom/svg.lisp
@@ -106,3 +106,74 @@
 (defmacro body (&body body)
   `(tag body ()
      ,@body))
+
+;; Generate all the svg node ceremony:
+;; CL-USER> (svg 400 500)
+;; <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="500" width="400"></svg>
+(defmacro svg (width height &body body)
+  `(tag svg (xmlns "http://www.w3.org/2000/svg"
+                   "xmlns:xlink" "http://www.w3.org/1999/xlink"
+                   height ,height
+                   width ,width)
+     ,@body))
+
+;; Manipulate color attrs:
+;; CL-USER> (brightness '(255 0 0) -100)
+;; (155 0 0)
+(defun brightness (col amt)
+  (mapcar (lambda (x)
+            (min 255 (max 0 (+ x amt))))
+          col))
+
+;; Manipulate style: make the outline a darker variant of the fill
+;; CL-USER> (svg-style '(255 0 0))
+;; "fill:rgb(255,0,0);stroke:rgb(155,0,0)"
+(defun svg-style (color)
+  (format nil
+          "~{fill:rgb(~a,~a,~a);stroke:rgb(~a,~a,~a)~}"
+          (append color
+                  (brightness color -100))))
+
+;; circle is just a function: we don't need the "decorating" power of a macro
+;; CL-USER> (circle '(50 . 50) 50 '(255 0 0))
+;; <circle cx="50" cy="50" r="50" style="fill:rgb(255,0,0);stroke:rgb(155,0,0)"></circle>
+(defun circle (center radius color)
+  (tag circle (cx (car center)
+               cy (cdr center)
+               r radius
+               style (svg-style color))))
+
+
+;;; EXAMPLE OF SVG DSL:
+
+(defun polygon (points color)
+  (tag polygon (points (format nil "~{~a,~a ~}"
+                               (mapcan (lambda (tp)
+                                         (list (car tp) (cdr tp)))
+                                       points))
+                       style (svg-style color))))
+
+;; CL-USER> (random-walk 100 10)
+;; (100 99 98 97 98 97 96 95 96 95)
+(defun random-walk (value length)
+  (unless (zerop length)
+    (cons value
+          (random-walk (if (zerop (random 2))
+                           (1- value)
+                           (1+ value))
+                       (1- length)))))
+
+;; draws a random graph
+(defun draw ()
+  (with-open-file (*standard-output* "random_walk.svg"
+                                     :direction :output
+                                     :if-exists :supersede)
+    (svg 400 200
+      (loop repeat 10
+            do (polygon (append '((0 . 200))
+                                (loop for x
+                                      for y in (random-walk 100 400)
+                                      collect (cons x y))
+                                '((400 . 200)))
+                        (loop repeat 3
+                              collect (random 256)))))))
