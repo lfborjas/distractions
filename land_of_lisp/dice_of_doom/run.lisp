2a226a3a0594fd91c367c739725d467dda03c1a8
Luis Borjas <luisfborjas@gmail.com>
2018-11-26 01:13:09 -0500

    some parting words

--- land_of_lisp/dice_of_doom/run.lisp
+++ land_of_lisp/dice_of_doom/run.lisp
@@ -1,10 +1,17 @@
 ;; Loads all the files in order of definition,
-;; since there's some stuff 
+;; since there's some stuff that was incrementally
+;; improved to illustrate lazy evaluation, memoization,
+;; chance nodes, and AI improvements like alpha-beta
+;; pruning, and chance nodes for overall better gameplay
 (load "dice_of_doom_v1.lisp")
+
 (load "lazy.lisp")
 (load "dice_of_doom_v2.lisp")
+
 (load "webserver.lisp")
 (load "svg.lisp")
 (load "dice_of_doom_v3.lisp")
+
 (load "dice_of_doom_v4.lisp")
+
 (serve #'dod-request-handler)
